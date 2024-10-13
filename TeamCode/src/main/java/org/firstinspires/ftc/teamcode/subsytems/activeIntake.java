51b3fd6cacca0da7152dd128d240564ad3173111
NikhilRao21 <suprnick1@gmail.com>
2024-10-13 14:18:28 -0400

    created getters/setters for intake status, created basic intake movement using crservo.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsytems/activeIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsytems/activeIntake.java
@@ -8,11 +8,39 @@ public class activeIntake {
     Gamepad gamepad2previous;
     CRServo intake;
 
+    public enum intakeState {FORWARD,BACKWARD,OFF}
+
+    public intakeState intakePos;
+    public intakeState getIntakePos() {
+        return intakePos;
+    }
+
+    public void setIntakePos(intakeState intakePos) {
+        this.intakePos = intakePos;
+    }
+
+
+
     public activeIntake(Gamepad gamepad2, Gamepad gamepad2previous, CRServo intake){
         this.gamepad2=gamepad2;
         this.gamepad2previous=gamepad2previous;
         this.intake=intake;
     }
 
-    
+    public void intakeForward() {
+        intake.setPower(1);
+        setIntakePos(intakeState.FORWARD);
+    }
+
+    public void intakeOff() {
+        intake.setPower(0);
+        setIntakePos(intakeState.OFF);
+    }
+
+    public void intakeBack() {
+        intake.setPower(-1);
+        setIntakePos(intakeState.BACKWARD);
+    }
+
+
 }
