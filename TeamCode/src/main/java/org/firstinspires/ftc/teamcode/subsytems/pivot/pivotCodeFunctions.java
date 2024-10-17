280c6ae4161bbb37dcc17e402a0d04846b3f03be
Deven Balaji <devenvbalaji@gmail.com>
2024-10-17 09:43:19 -0400

    Integrated pivot code into the Teleop, and made the pivot functions more portable.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsytems/pivot/pivotCodeFunctions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsytems/pivot/pivotCodeFunctions.java
@@ -6,18 +6,20 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 public class pivotCodeFunctions {
     DcMotorEx pivot;
     int pivotPos;
+    int topPos;
     PivotPIDFFunctions functions;
-    public pivotCodeFunctions(DcMotorEx pivot,PivotPIDFFunctions functions){
+    public pivotCodeFunctions(DcMotorEx pivot,PivotPIDFFunctions functions, int topPos){
         this.pivot=pivot;
         this.functions=functions;
+        this.topPos = topPos;
     }
     public void goTo(int targetPos){
         pivotPos = targetPos;
         if (pivotPos < 0){
             pivotPos = 0;
         }
-        if (pivotPos > 2187){
-            pivotPos = 2187;
+        if (pivotPos > topPos){
+            pivotPos = topPos;
         }
         functions.moveToPos(pivot.getCurrentPosition(), targetPos);
     }
