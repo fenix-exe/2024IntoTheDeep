9bed39cca25b26424f27a4a0d0b1253f2d139db1
NikhilRao21 <nvsrao09@gmail.com>
2024-10-19 14:52:48 -0400

    coded differential, added error, need to add in teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsytems/differential.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsytems/differential.java
@@ -1,19 +1,37 @@
 package org.firstinspires.ftc.teamcode.subsytems;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.Servo;
-
+@Config
 public class differential {
     Servo left;
     Servo right;
 
     float leftRange = 270;
     float rightRange = 270;
-    float pitchError = 7;
-    float rollError = 0;
+    public static float pitchError = 7;
+    public static float rollError = 20;
 
     public differential(Servo left, Servo right){
         this.left = left;
         this.right = right;
+    }
+
+    private double setAPosition(double pitch, double roll) {
+        double aPos;
+        aPos = (1 / leftRange) * (pitch + roll / 2) + 0.5;
+        return aPos;
+    }
+
+    private double setBPosition(double pitch, double roll) {
+        double bPos;
+        bPos = (1 / rightRange) * (pitch - roll / 2) + 0.5;
+        return bPos;
+    }
+
+    public void setDifferentialPosition(double pitch, double roll){
+        left.setPosition(setAPosition(pitch+pitchError, roll+rollError));
+        right.setPosition(setBPosition(pitch+pitchError, roll+rollError));
     }
     
 
