4ad939b5fe7025f787d4b43af6ded6f75e8fee5a
NikhilRao21 <nvsrao09@gmail.com>
2024-11-09 17:44:22 -0500

    added active intake code to diffteleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/testCode/diffTELEOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/testCode/diffTELEOP.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.util.testCode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.subsytems.DriverControls;
@@ -12,16 +13,19 @@ public class diffTELEOP extends LinearOpMode {
     Servo left;
     Servo right;
     differential diffCode;
+    CRServo intake;
     DriverControls controls;
     @Override
     public void runOpMode() throws InterruptedException {
         left = hardwareMap.get(Servo.class,"left");
         right = hardwareMap.get(Servo.class, "right");
+        intake = hardwareMap.get(CRServo.class, "intake");
         diffCode = new differential(left, right);
 
         waitForStart();
         while (opModeIsActive()) {
             diffCode.setDifferentialPosition(-90, 0);
+            intake.setPower(-1);
             telemetry.addData("left", left.getPosition());
             telemetry.addData("g", gamepad2.left_stick_y);
             telemetry.addData("g2", gamepad2.right_stick_y);
