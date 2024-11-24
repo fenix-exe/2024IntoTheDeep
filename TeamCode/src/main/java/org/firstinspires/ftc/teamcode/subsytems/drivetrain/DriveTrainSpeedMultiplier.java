3f3680339c5a6f80cfea45d603d37159c41f5ed5
Deven Balaji <devenvbalaji@gmail.com>
2024-11-23 21:22:39 -0500

    made drivetrain faster when ultra slow is triggered

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsytems/drivetrain/DriveTrainSpeedMultiplier.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsytems/drivetrain/DriveTrainSpeedMultiplier.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode.subsytems.drivetrain;
 
 public class DriveTrainSpeedMultiplier {
-    public static double SUPER_SLOW = 0.2;
+    public static double SUPER_SLOW = 0.4;
     public static double HALF_SPEED = 0.5;
     public static double NO_MULTIPLIER = 1;
 
