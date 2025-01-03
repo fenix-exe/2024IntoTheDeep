b4e47cbb5b7c4cbc36174c37198acd4f8bc23ab8
Deven Balaji <devenvbalaji@gmail.com>
2025-01-02 22:53:33 -0500

    created framework for reading state model params from file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/stateModels/PresetConfigUtil.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/stateModels/PresetConfigUtil.java
@@ -14,66 +14,30 @@ import java.io.PrintWriter;
 
 public class PresetConfigUtil {
     public static String PRESETFILE = "/sdcard/Download/TeleOpV5/PresetPositions.csv";
-    public static void loadPresetsFromConfig(){
-
+    public static int loadPresetsFromConfig(){
+        int count  = 0;   // counts how many different preset types we read from the file
         try (BufferedReader reader = new BufferedReader(new FileReader(PRESETFILE))){
             String line;
             while ((line = reader.readLine()) != null){
-                PresetPositions presetPosition = PresetConfigUtil.fromString(line);
-                if(presetPosition != null) {
-                    updatePresetPosition(presetPosition);
-                }
+                updatePresetPosition(line.split(","));
+                count++;
             }
         } catch (Exception e){
             LoggerUtil.logException("updatePresets",e);
         }
+        return count;
     }
-    private static void updatePresetPosition(PresetPositions presetPosition){
-        switch (presetPosition.getName()) {
-            case INTAKE:
-                PresetPositions.INTAKE_POSITION = presetPosition;
-                break;
-            case DEPOSIT_FRONT_TOP:
-                PresetPositions.DEPOSIT_FRONT_TOP_POSITION = presetPosition;
-                break;
-            case SAFE_DRIVE:
-                PresetPositions.SAFE_DRIVING_POSITION = presetPosition;
+    private static void updatePresetPosition(String[] configValues){
+        String preset_name = configValues[0];
+        switch (preset_name) {
+            case "DRIVE_STATE_MODEL_PARAMS":
+                StateModelParameters.DriveStateParameters.pitch = Double.parseDouble(configValues[1]);
+                StateModelParameters.DriveStateParameters.elbowAngle = Double.parseDouble(configValues[2]);
+                StateModelParameters.DriveStateParameters.slideLength = Double.parseDouble(configValues[3]);
                 break;
             default:
-                LoggerUtil.error("updatePresets", "Read Unknown Preset," + presetPosition.toString());
+                LoggerUtil.error("updatePresets", "Read Unknown Preset," + preset_name );
                 break;
-        }
-    }
-    private static PresetPositions fromString(String csv){
-        String[] readValues=csv.split(",");
-        try {
-            PresetPositions.PositionName name = PresetPositions.PositionName.valueOf(readValues[0]);
-            double pitch = Double.parseDouble(readValues[1]);
-            double roll = Double.parseDouble(readValues[2]);
-            double elbowAngle = Double.parseDouble(readValues[3]);
-            double slideExtension = Double.parseDouble(readValues[4]);
-            return new PresetPositions(name, pitch, roll, elbowAngle, slideExtension);
-        }catch(Exception e){
-            LoggerUtil.logException("updatePresets",e);
-        }
-        return null;
-    }
-    public static void writePresetsToConfig(){
-        File file = new File(PRESETFILE);
-        try{
-            file.getParentFile().mkdirs();
-            file.createNewFile();
-            PrintWriter writer = new PrintWriter(new FileOutputStream(file, false));
-
-            //write presets
-            writer.println(PresetPositions.INTAKE_POSITION.toString());
-            writer.println(PresetPositions.DEPOSIT_FRONT_TOP_POSITION.toString());
-            writer.println(PresetPositions.SAFE_DRIVING_POSITION.toString());
-
-            //close writer
-            writer.close();
-        } catch (Exception e){
-            LoggerUtil.logException("updatePresets",e);
         }
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/stateModels/StateModelParameters.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.stateModels;
+
+public class StateModelParameters {
+
+    public static class DriveStateParameters{
+        public static double pitch = 0;
+        public static double elbowAngle = 58;
+        public static double slideLength = 8;
+    }
+
+}
