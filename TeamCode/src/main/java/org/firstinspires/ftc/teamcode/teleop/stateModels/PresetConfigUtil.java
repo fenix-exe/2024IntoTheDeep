033ab1823e0d21dbcf8a7825dac9363da91a5d00
Deven Balaji <devenvbalaji@gmail.com>
2025-01-08 11:51:49 -0500

    added preset reading from file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/stateModels/PresetConfigUtil.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/stateModels/PresetConfigUtil.java
@@ -28,6 +28,71 @@ public class PresetConfigUtil {
                 StateModelParameters.DriveStateParameters.elbowAngle = Double.parseDouble(configValues[2]);
                 StateModelParameters.DriveStateParameters.slideLength = Double.parseDouble(configValues[3]);
                 break;
+            case "INTAKE_STATE_MODEL_PARAMS":
+                StateModelParameters.IntakeStateParameters.pitch = Double.parseDouble(configValues[1]);
+                StateModelParameters.IntakeStateParameters.roll = Double.parseDouble(configValues[2]);
+                StateModelParameters.IntakeStateParameters.downPitch = Double.parseDouble(configValues[3]);
+                StateModelParameters.IntakeStateParameters.downRoll = Double.parseDouble(configValues[4]);
+                StateModelParameters.IntakeStateParameters.elbowAngle = Double.parseDouble(configValues[5]);
+                StateModelParameters.IntakeStateParameters.slideLength = Double.parseDouble(configValues[6]);
+                break;
+            case "DEPOSIT_STATE_MODEL_PARAMS":
+                StateModelParameters.DepositStateParameters.pitch = Double.parseDouble(configValues[1]);
+                StateModelParameters.DepositStateParameters.roll = Double.parseDouble(configValues[2]);
+                StateModelParameters.DepositStateParameters.elbowAngle = Double.parseDouble(configValues[3]);
+                StateModelParameters.DepositStateParameters.slideLength = Double.parseDouble(configValues[4]);
+                break;
+            case "DEPOSIT_BACK_STATE_MODEL_PARAMS":
+                StateModelParameters.DepositBackStateParameters.pitch = Double.parseDouble(configValues[1]);
+                StateModelParameters.DepositBackStateParameters.roll = Double.parseDouble(configValues[2]);
+                StateModelParameters.DepositBackStateParameters.elbowAngle = Double.parseDouble(configValues[3]);
+                StateModelParameters.DepositBackStateParameters.slideLength = Double.parseDouble(configValues[4]);
+                StateModelParameters.DepositBackStateParameters.slideRetractionLength = Double.parseDouble(configValues[5]);
+                break;
+            case "DEPOSIT_SAMPLE_INTO_BUCKET_STATE_MODEL_PARAMS":
+                StateModelParameters.DepositSampleIntoBucketStateParameters.pitch = Double.parseDouble(configValues[1]);
+                StateModelParameters.DepositSampleIntoBucketStateParameters.roll = Double.parseDouble(configValues[2]);
+                StateModelParameters.DepositSampleIntoBucketStateParameters.elbowAngle = Double.parseDouble(configValues[3]);
+                StateModelParameters.DepositSampleIntoBucketStateParameters.slideLength = Double.parseDouble(configValues[4]);
+                break;
+            case "GRAB_FROM_OUTSIDE_STATE_MODEL_PARAMS":
+                StateModelParameters.GrabBlockFromOutsideStateParameters.downPitch = Double.parseDouble(configValues[1]);
+                StateModelParameters.GrabBlockFromOutsideStateParameters.upPitch= Double.parseDouble(configValues[2]);
+                StateModelParameters.GrabBlockFromOutsideStateParameters.upRoll = Double.parseDouble(configValues[3]);
+                StateModelParameters.GrabBlockFromOutsideStateParameters.elbowIntakeDownAngle = Double.parseDouble(configValues[4]);
+                StateModelParameters.GrabBlockFromOutsideStateParameters.elbowIntakeUpAngle = Double.parseDouble(configValues[5]);
+                StateModelParameters.GrabBlockFromOutsideStateParameters.elbowAngle = Double.parseDouble(configValues[6]);
+                StateModelParameters.GrabBlockFromOutsideStateParameters.slideLength = Double.parseDouble(configValues[7]);
+                break;
+            case "GRAB_FROM_INSIDE_STATE_MODEL_PARAMS":
+                StateModelParameters.GrabBlockFromInsideStateParameters.downPitch = Double.parseDouble(configValues[1]);
+                StateModelParameters.GrabBlockFromInsideStateParameters.upPitch= Double.parseDouble(configValues[2]);
+                StateModelParameters.GrabBlockFromInsideStateParameters.upRoll = Double.parseDouble(configValues[3]);
+                StateModelParameters.GrabBlockFromInsideStateParameters.elbowDownAngle = Double.parseDouble(configValues[4]);
+                StateModelParameters.GrabBlockFromInsideStateParameters.elbowUpAngle = Double.parseDouble(configValues[5]);
+                StateModelParameters.GrabBlockFromInsideStateParameters.elbowAngle = Double.parseDouble(configValues[6]);
+                StateModelParameters.GrabBlockFromInsideStateParameters.slideLength = Double.parseDouble(configValues[7]);
+                break;
+            case "PICKUP_SPECIMENS_STATE_MODEL_PARAMS":
+                StateModelParameters.PickupSpecimensStateParameters.pitch = Double.parseDouble(configValues[1]);
+                StateModelParameters.PickupSpecimensStateParameters.roll= Double.parseDouble(configValues[2]);
+                StateModelParameters.PickupSpecimensStateParameters.elbowAngle = Double.parseDouble(configValues[3]);
+                StateModelParameters.PickupSpecimensStateParameters.slideLength = Double.parseDouble(configValues[4]);
+                StateModelParameters.PickupSpecimensStateParameters.elbowUpAngle = Double.parseDouble(configValues[5]);
+                StateModelParameters.PickupSpecimensStateParameters.endSlideLength = Double.parseDouble(configValues[6]);
+                StateModelParameters.PickupSpecimensStateParameters.pitchEnd = Double.parseDouble(configValues[7]);
+                StateModelParameters.PickupSpecimensStateParameters.rollEnd = Double.parseDouble(configValues[8]);
+                break;
+            case "DEPOSIT_SPECIMENS_STATE_MODEL_PARAMS":
+                StateModelParameters.DepositSpecimensStateParameters.pitch = Double.parseDouble(configValues[1]);
+                StateModelParameters.DepositSpecimensStateParameters.roll= Double.parseDouble(configValues[2]);
+                StateModelParameters.DepositSpecimensStateParameters.elbowAngle = Double.parseDouble(configValues[3]);
+                StateModelParameters.DepositSpecimensStateParameters.elbowDownAngle = Double.parseDouble(configValues[4]);
+                StateModelParameters.DepositSpecimensStateParameters.slideStartLength = Double.parseDouble(configValues[5]);
+                StateModelParameters.DepositSpecimensStateParameters.slideDepositLength = Double.parseDouble(configValues[6]);
+                break;
+            case "DROP_BLOCK_AND_MOVE_WRIST_DOWN":
+                StateModelParameters.DropBlockAndMoveWristDown.pitch = Double.parseDouble(configValues[1]);
             default:
                 LoggerUtil.error("updatePresets", "Read Unknown Preset," + preset_name );
                 break;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/stateModels/StateModelParameters.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/stateModels/StateModelParameters.java
@@ -7,5 +7,72 @@ public class StateModelParameters {
         public static double elbowAngle = 58;
         public static double slideLength = 8;
     }
+    public static class IntakeStateParameters{
+        public static double pitch=0;
+        public static double roll=-90;
+        public static double downPitch=-90;
+        public static double downRoll=0;
+        public static double elbowAngle=12;
+        public static double slideLength=12;
+    }
+    public static class DepositStateParameters{
+        public static double pitch = -30;
+        public static double roll = 0;
+        public static double elbowAngle = 73;
+        public static double slideLength = 30.5;
+    }
+    public static class DepositBackStateParameters{
+        public static double pitch = 75;
+        public static double roll = 0;
+        public static double elbowAngle = 87;
+        public static double slideLength = 24;
+        public static double slideRetractionLength = 8;
+    }
+    public static class DepositSampleIntoBucketStateParameters{
+        public static double pitch = 0;
+        public static double roll = 0;
+        public static double elbowAngle = 58;
+        public static double slideLength = 8;
+    }
+    public static class GrabBlockFromOutsideStateParameters{
+        public static double downPitch = -90;
+        public static double upPitch = 0;
+        public static double upRoll = 0;
+        public static double elbowIntakeDownAngle = 4;
+        public static double elbowIntakeUpAngle = 10;
+        public static double elbowAngle = 58;
+        public static double slideLength = 0;
+    }
+    public static class GrabBlockFromInsideStateParameters{
+        public static double downPitch = -90;
+        public static double upPitch = 0;
+        public static double upRoll = -90;
+        public static double elbowDownAngle = 2;
+        public static double elbowUpAngle = 10;
+        public static double elbowAngle = 58;
+        public static double slideLength = 0;
+
+    }
+    public static class PickupSpecimensStateParameters{
+        public static double pitch = -10;
+        public static double roll = 90;
+        public static double elbowAngle = 25;
+        public static double slideLength = 0;
+        public static double elbowUpAngle = 77;
+        public static double endSlideLength = 3;
+        public static double pitchEnd = 90;
+        public static double rollEnd = 90;
+    }
+    public static class DepositSpecimensStateParameters{
+        public static double pitch = 90;
+        public static double roll = 90;
+        public static double elbowAngle = 77;
+        public static double elbowDownAngle = 58;
+        public static double slideStartLength = 3;
+        public static double slideDepositLength = 16;
+    }
+    public static class DropBlockAndMoveWristDown{
+        public static double pitch = -90
+    }
 
 }
