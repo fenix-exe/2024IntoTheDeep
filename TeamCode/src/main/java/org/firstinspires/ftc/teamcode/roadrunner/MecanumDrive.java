ed6b3cc7adee1d3ddc7e0b27c1e2744bf4118e02
NikhilRao21 <nvsrao09@gmail.com>
2025-01-09 18:11:34 -0500

    auto added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
@@ -84,7 +84,7 @@ public class MecanumDrive {
         // path controller gains
         public double axialGain = 1.0;
         public double lateralGain = 7.0;
-        public double headingGain = 8.0; // shared with turn
+        public double headingGain = 3.0; // shared with turn
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
