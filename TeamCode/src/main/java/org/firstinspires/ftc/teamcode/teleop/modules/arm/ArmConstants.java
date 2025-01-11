6faf6c540e07f9ba2060a03c5bdcc7ac226b98d7
Deven Balaji <devenvbalaji@gmail.com>
2025-01-10 19:15:20 -0500

    4 more inches of extension

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/modules/arm/ArmConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/modules/arm/ArmConstants.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.teleop.modules.arm;
 public class ArmConstants {
     public static int ELBOWPRESETTOLERANCE = 3;
     public static int SLIDEPRESETTOLERANCE = 1;
-    public static int MAXSLIDEEXTENSIONLENGTHINCHES = 15;
+    public static int MAXSLIDEEXTENSIONLENGTHINCHES = 19;
     public static int ELBOWINTERMEDIATETHRESHOLD = 23;
     public static int ELBOWBOTTOMANGLE = -5;
     public static int ELBOWTICKSTOLERANCE = 100;
