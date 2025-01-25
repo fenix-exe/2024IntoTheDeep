228fee8a67b6922db5c6bfd8f548c2601f7ad627
Deven Balaji <devenvbalaji@gmail.com>
2025-01-24 23:34:54 -0500

    disabled specimen teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/opmodes/TeleOpV5SpecimenZapdos.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/opmodes/TeleOpV5SpecimenZapdos.java
@@ -6,6 +6,7 @@ import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.ftc.GoBildaPinpointDriverRR;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.hardware.rev.RevTouchSensor;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -42,6 +43,7 @@ import java.util.HashMap;
 
 @Config
 @TeleOp
+@Disabled
 public class TeleOpV5SpecimenZapdos extends LinearOpMode {
     MultipleTelemetry multiTelemetry;
     DriveTrain driveTrain;
