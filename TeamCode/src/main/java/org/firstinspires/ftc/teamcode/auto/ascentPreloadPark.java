2f372b559bb2d6aa7c0419c37f8e7c925530ed1d
NikhilRao21 <nvsrao09@gmail.com>
2025-02-02 18:04:50 -0500

    auto fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/ascentPreloadPark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/ascentPreloadPark.java
@@ -181,10 +181,10 @@ public class ascentPreloadPark extends LinearOpMode {
         elbowMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         elbowMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        elbowMotor.setTargetPosition(-217);
+        elbowMotor.setTargetPosition(-277);
         elbowMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         elbowMotor.setPower(0.2);
-        while (elbowMotor.getCurrentPosition() > -205) {
+        while (elbowMotor.getCurrentPosition() > -265) {
 
         }
         elbowMotor.setPower(0);
