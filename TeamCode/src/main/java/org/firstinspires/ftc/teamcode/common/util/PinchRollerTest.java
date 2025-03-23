ee8a68554226743b6ccdfe4e34b17cbb91920ee4
Deven Balaji <devenvbalaji@gmail.com>
2025-03-23 14:15:30 -0400

    added @teleop call

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/util/PinchRollerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/util/PinchRollerTest.java
@@ -1,10 +1,11 @@
 package org.firstinspires.ftc.teamcode.common.util;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 
 import org.firstinspires.ftc.teamcode.common.PinchRollerIntake;
-
+@TeleOp
 public class PinchRollerTest extends LinearOpMode {
     CRServo servo;
     PinchRollerIntake intake;
