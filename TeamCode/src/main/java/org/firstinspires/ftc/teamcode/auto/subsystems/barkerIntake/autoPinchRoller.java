b9404d3908640a6e68f83243c64fad606be5522e
NikhilRao21 <nvsrao09@gmail.com>
2025-03-23 16:05:28 -0400

    migrate from barker to pinch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/subsystems/barkerIntake/autoPinchRoller.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/subsystems/barkerIntake/autoPinchRoller.java
@@ -3,8 +3,6 @@ package org.firstinspires.ftc.teamcode.auto.subsystems.barkerIntake;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.ftc.DownsampledWriter;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.CRServoImpl;
 import com.qualcomm.robotcore.hardware.CRServoImplEx;
 
 import org.firstinspires.ftc.teamcode.auto.roadrunner.messages.ActiveIntakeMessage;
@@ -12,36 +10,36 @@ import org.firstinspires.ftc.teamcode.common.PinchRollerIntake;
 
 import androidx.annotation.NonNull;
 
-public class autoBarkerIntake extends PinchRollerIntake {
+public class autoPinchRoller extends PinchRollerIntake {
 
-    private final DownsampledWriter barkerWriter;
+    private final DownsampledWriter rollerWriter;
 
-    public autoBarkerIntake(CRServoImplEx intake) {
+    public autoPinchRoller(CRServoImplEx intake) {
         super(intake);
-        barkerWriter = new DownsampledWriter("BARKER INTAKE INFO", 50_000_000);
+        rollerWriter = new DownsampledWriter("BARKER INTAKE INFO", 50_000_000);
     }
 
     public void setPower(double power) {
         intake.setPower(power);
     }
 
-    public class barkerIntakePower implements Action {
+    public class pinchRollerPower implements Action {
         private final double power;
 
-        public barkerIntakePower(double power) {
+        public pinchRollerPower(double power) {
             this.power = power;
         }
 
         @Override
         public boolean run(@NonNull TelemetryPacket telemetryPacket) {
             setPower(power);
-            barkerWriter.write(new ActiveIntakeMessage(power));
+            rollerWriter.write(new ActiveIntakeMessage(power));
             return false;
         }
     }
 
-    public Action barkerIntakePower(double power) {
-        return new barkerIntakePower(power);
+    public Action pinchRollerPower(double power) {
+        return new pinchRollerPower(power);
     }
 
 }
