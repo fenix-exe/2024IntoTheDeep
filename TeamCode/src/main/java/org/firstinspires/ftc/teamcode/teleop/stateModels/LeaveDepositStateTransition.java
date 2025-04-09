7a4c46ef16d83e7e0c321c12c9c2a6a3e2e3011b
Deven Balaji <devenvbalaji@gmail.com>
2025-04-09 15:14:07 -0400

    made the sample eject at human player not a hold

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/stateModels/LeaveDepositStateTransition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/stateModels/LeaveDepositStateTransition.java
@@ -41,7 +41,7 @@ public class LeaveDepositStateTransition implements IStateTransition{
         switch(steps){
             case START:
                 if ((controls.depositBack() && FSMManager.robotState == RobotState.READY_TO_DEPOSIT_IN_BUCKET)
-                        || (!controls.specimenSampleIntake() && FSMManager.robotState == RobotState.READY_TO_DEPOSIT_TO_HUMAN_PLAYER)){
+                        || (controls.specimenSampleIntake() && FSMManager.robotState == RobotState.READY_TO_DEPOSIT_TO_HUMAN_PLAYER)){
                     FSMManager.stopTransitions();
                     timer.reset();
                     intake.slowOuttake();
