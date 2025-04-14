62ee5c43071bd981e23e96d2fda872d9e01fc342
NikhilRao21 <nvsrao09@gmail.com>
2025-04-14 12:19:15 -0400

    driver 1 cannot manually extend slides

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/modules/driverControl/DriverControlsDriver1Master.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/modules/driverControl/DriverControlsDriver1Master.java
@@ -22,9 +22,9 @@ public class DriverControlsDriver1Master extends DriverControls{
         if (Math.abs(gamepad2current.left_stick_y) > 0.65){
             return -gamepad2current.left_stick_y;
         }
-        if (Math.abs(gamepad1current.right_stick_y) > 0.65){
+        /*if (Math.abs(gamepad1current.right_stick_y) > 0.65){
             return -gamepad1current.right_stick_y;
-        }
+        }*/
         return 0;
     }
     @Override
